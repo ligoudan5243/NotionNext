e4adf2369e6f876f8f223fa52a8cdfbd46ace047
ligoudan5243 <150013646+ligoudan5243@users.noreply.github.com>
2024-11-05 17:10:42 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,10 +24,10 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Mystery Q', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Li God', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '屎上最强博客👍', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'http://nblogs.qbobo.eu.org', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Mystery Q‘s Blog', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || 'http://nblogs.ro22.eu.org', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Li God‘s Blog', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
